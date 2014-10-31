02ae7e67b1c44781eaecab53fc70c7f651060228
Mattis <git@mcom.moepl.eu>
2014-10-31 13:53:00 +0100

    Changed the example from Kiril a bit so it should mostly fullfill the given specification

--- app/views/login.py
+++ app/views/login.py
@@ -1,7 +1,21 @@
-# -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from app.models.file import File
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 
-
+# see
+# https://docs.djangoproject.com/en/dev/topics/auth/default/#django.contrib.auth.login
 def index(request):
-    return render(request, 'login.html', {'username': ''})
+    if request.method == 'POST':
+        username = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                # Redirect to a success page. Currently just our "index.html" site
+                #TODO direct to the correct success page (does no exist yet)
+                return redirect('/')
+        else:
+            messages.error(request,'Email/password incorrect')
+    return render(request, 'login.html')
