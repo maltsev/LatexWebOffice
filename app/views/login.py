84e545980ec5508d0725ab586dd39f8d27385535
mattis <m_stef16@wwu.de>
2014-11-04 21:28:15 +0100

    Added doxygen comment for the login handler

--- app/views/login.py
+++ app/views/login.py
@@ -5,6 +5,9 @@ from django.contrib import messages
 
 # see
 # https://docs.djangoproject.com/en/dev/topics/auth/default/#django.contrib.auth.login
+
+##  Default handler for login requests by the client that sends the client the login page. If correct login details were sent with the request (over POST data), the user will be redirected to a success page. Otherwise an error message will be inserted into the django messages queue. 
+#   @param request The HttpRequest Object
 def index(request):
     if request.method == 'POST':
         username = request.POST['email']
@@ -16,6 +19,10 @@ def index(request):
                 # Redirect to a success page. Currently just our "index.html" site
                 #TODO direct to the correct success page (does no exist yet)
                 return redirect('/')
+            #else:
+                #Some handling because the user is banned/email was not verified yet?
+                
+               
         else:
             messages.error(request,'Email/password incorrect')
     return render(request, 'login.html')
