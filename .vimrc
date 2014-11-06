909a25d34beba26588f54fc475a70a51a7024fa6
Mattis <git@mcom.moepl.eu>
2014-11-06 12:52:51 +0100

    Added a -default header for python files -.vimrc for automatically using the header - tests folder with a testclass for logins made manage.py executeable

---
+++ .vimrc
@@ -0,0 +1,7 @@
+"http://www.thegeekstuff.com/2008/12/vi-and-vim-autocommand-3-steps-to-add-custom-header-to-your-file/
+autocmd bufnewfile *.py so ./py_header.txt
+autocmd bufnewfile *.py exe "1," . 10 . "g/Creation Date :.*/s//Creation Date : " .strftime("%d-%m-%Y")
+autocmd bufnewfile *.py exe "1," . 10 . "g/Author :.*/s//Author : "  $USER
+autocmd Bufwritepre,filewritepre *.py execute "normal ma"
+autocmd Bufwritepre,filewritepre *.py exe "1," . 10 . "g/Last Modified :.*/s/Last Modified :.*/Last Modified : " .strftime("%c")
+autocmd bufwritepost,filewritepost *.py execute "normal `a"
---
+++ app/tests/test_LoginRegistration.py
@@ -0,0 +1,56 @@
+""" 
+
+* Purpose :
+
+* Creation Date : 06-11-2014
+
+* Last Modified : Do 06 Nov 2014 12:18:08 CET
+
+* Author :  mattis
+
+* Coauthors :   
+
+"""
+
+
+from django.test import TestCase,Client
+from django.contrib.auth.models import User
+
+
+class LoginTestClass(TestCase):
+
+    """Docstring for LoginTestClass. """
+
+## Setup method. It constructs two users and a Client object to do requests with: 
+# user1active - an active user
+# user2inactive - an inactive user
+# client - the client to do requests with
+    def setUp(self):
+        user1active=User.objects.create_user('test@test.com',password='123456')
+        user1active.save()
+        self._user1active=user1active
+
+        user2inactive=User.objects.create_user('test2@test.com',password='none')
+        user2inactive.is_active=False
+        user2inactive.save()
+        self._user2inactive=user2inactive
+        self._client=Client()
+
+## Tests if on incorrect login the client received a failure message and is directed to the login page
+    def test_loginFails(self):
+        #Testing that a user is not logged in with an incorrect password
+        response=self._client.post('/login/',{'email':self._user1active.username,'password':'wrong'})
+        self.assertNotIn('_auth_user_id', self.client.session)
+
+        #Testing that an user receives a failure message on incorrect login        
+
+        #Testing that an user is directed to the login page again
+
+    
+    def test_loginSuccess(self):
+
+        #Testing that a user is logged in with an correct password
+
+        #Testing that a user is directed tothe "Startseite" after an correct password
+
+        pass
