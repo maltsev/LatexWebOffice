7a829179de8830ef3be02fbedf6f2181be7bcbd4
munzirm <m_moha01@uni-muenster.de>
2014-11-07 13:55:07 +0100

    Überprüfung auf leere eMail bei der Anmeldung

---
+++ app/static/js/inputvalidation.js
@@ -0,0 +1,21 @@
+// Überprüft Formular-Daten und zeigt ggf. entsprechende Fehlermeldungen an
+function validateForm(id) {
+	// Eingabefelder des Formulars einlesen
+	var formElements = document.getElementById(id).getElementsByTagName('input');
+
+	// Werte der Eingabefelder überprüfen
+	for (var i = 0; i < formElements.length; ++i) {
+		var element = formElements[i];
+
+		// eMail-Adresse
+		if (element.type == 'email') {
+			if (!/(.+)@(.+){2,}\.(.+){2,}/.test(element.value))
+				element.setCustomValidity('Bitte eine gültige eMail-Adresse angeben');
+			else
+				element.setCustomValidity('');
+		}
+	}
+
+	// TODO: nur submit-Buttons finden
+	document.getElementById(id).getElementsByTagName('button')[0].click();
+}
