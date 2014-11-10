55a088e3eb4b4b8db2272699539abb1c8ad31890
Christian Lohmann <c_lohm05@wwu.de>
2014-11-10 10:43:44 +0100

    added registration added error message field in registration.html (same as login) added tests for registration

--- app/tests/server/test_LoginRegistration.py
+++ app/tests/server/test_LoginRegistration.py
@@ -1,14 +1,14 @@
 """ 
 
-* Purpose :
+* Purpose : Testing of essential registration and login functionality
 
 * Creation Date : 06-11-2014
 
 * Last Modified : Thu 06 Nov 2014 07:20:54 PM CET
 
-* Author :  mattis
+* Author : mattis
 
-* Coauthors :   
+* Coauthors : christian
 
 * Sprintnumber : 1 
     
@@ -18,16 +18,14 @@
 from django.test import TestCase,Client
 from django.contrib.auth.models import User
 
-
 class LoginTestClass(TestCase):
 
     """Docstring for LoginTestClass. """
 
-# Setup method. It constructs two users and a Client object to do requests with:
-# user1active - an active user
-# user2inactive - an inactive user
-# client - the client to do requests with
-
+    # Setup method. It constructs two users and a client object to do requests with:
+    # user1active - an active user
+    # user2inactive - an inactive user
+    # client - the client to do requests with
     def setUp(self):
         user1active = User.objects.create_user(
             'test@test.com', password='123456')
@@ -39,14 +37,14 @@ class LoginTestClass(TestCase):
         user2inactive.save()
         self._user2inactive = user2inactive
         self._client = Client()
-# Tests if on incorrect login the client received a failure message and is
-# directed to the login page
 
+    # Tests if on incorrect login the client received a failure message and is
+    # directed to the login page
     def test_loginFails(self):
         # Testing that a user is not logged in with an incorrect password
         response = self._client.post(
             '/login/', {'email': self._user1active.username, 'password': 'wrong'})
-        self.assertNotIn('_auth_user_id', self.client.session)
+        self.assertNotIn('_auth_user_id', self._client.session)
 
         # Testing that an user receives a failure message on incorrect login
 
@@ -56,7 +54,7 @@ class LoginTestClass(TestCase):
 
         # Testing that a user is logged in with an correct password
 
-        # Testing that a user is directed tothe "Startseite" after an correct
+        # Testing that a user is directed to the "Startseite" after an correct
         # password
 
         pass
@@ -67,27 +65,78 @@ class RegistrationTestClass(TestCase):
 
     """Docstring for RegistrationTestClass. """
 
+    # Setup method. It constructs 4 user data sets and a client object to do requests with:
+    # user1 - should register and login successfully; second registration with same email should fail
+    # user2 - incorrect email address, registration should fail
+    # user3 - one blank field, registration should fail
+    # user4 - different password, registration should fail
+    #  client - the client to do requests with
     def setUp(self):
+
+        # user 1 data -> everything correct
+        self.user1_first_name = 'user1'
+        self.user1_email = 'user1@test.de'
+        self.user1_password1 = 'test123'
+        self.user1_password2 = 'test123'
+
+        # user 2 data -> incorrect email address (username)
+        self.user2_first_name = 'user2'
+        self.user2_email = 'user2@test'
+        self.user2_password1 = 'test1234'
+        self.user2_password2 = 'test1234'
+
+        # user 3 data -> contains a blank field
+        self.user3_first_name = ''
+        self.user3_email = 'user3@test.de'
+        self.user3_password1 = 'test'
+        self.user3_password2 = 'test'
+
+        # user 4 data -> passwords do not match
+        self.user4_first_name = 'user4'
+        self.user4_email = 'user3@test.de'
+        self.user4_password1 = 'test123'
+        self.user4_password2 = 'test12'
+
         self._client = Client()
 
-# Tests that you successfully register when you fill out all fields and
-# enter a valid email address
-    def test_registerSuccess(self):
-        pass
+    # Test if you successfully register when you fill out all fields and
+    # enter a valid email address -> user1
+    # if automatic login after registration was successful this test passes
+    def test_registrationSuccess(self):
+        response = self._client.post(
+            '/registration/', {'first_name': self.user1_first_name, 'email': self.user1_email,
+            'password1': self.user1_password1, 'password2': self.user1_password2})
+        self.assertIn('_auth_user_id', self._client.session)
 
-# Tests that you can't register with an invalid email address
-    def test_loginFalseInvalidEmail(self):
-        pass
+    # Test if you can't register when same email is already registered
+    def test_registrationFailedAlreadyRegistered(self):
+        # create user1 separately again, because user1 from first registrationSuccess
+        # doesn't exist here in our database
+        new_user = User.objects.create_user(username=self.user1_email,
+                                               email=self.user1_email, password=self.user1_password1,
+                                               first_name=self.user1_first_name)
+        response = self._client.post(
+            '/registration/', {'first_name': self.user1_first_name, 'email': self.user1_email,
+            'password1': self.user1_password1, 'password2': self.user1_password2}, follow=True)
+        self.assertContains(response, 'E-Mail-Adresse ist bereits registriert.')
 
-# Tests that you can't register when you didn't fill out all fields
-    def test_loginFalseNotFilledAllFields(self):
-        pass
+    # Test if you can't register with an invalid email address -> user 2
+    def test_registrationFailedInvalidEmail(self):
+        response = self._client.post(
+            '/registration/', {'first_name': self.user2_first_name, 'email': self.user2_email,
+            'password1': self.user2_password1, 'password2': self.user2_password2})
+        self.assertContains(response, 'Ungültige E-Mail-Adresse')
 
+    # Test if you can't register when you didn't fill out all fields -> user3
+    def test_registrationFailedNotFilledAllFields(self):
+        response = self._client.post(
+            '/registration/', {'first_name': self.user3_first_name, 'email': self.user3_email,
+            'password1': self.user3_password1, 'password2': self.user3_password2})
+        self.assertContains(response, 'Keine leeren Eingaben erlaubt.')
 
-
-
-
-
-
-
-
+    # Test if you can't register when the passwords do not match
+    def test_registrationFailedPasswordsDontMatch(self):
+        response = self._client.post(
+            '/registration/', {'first_name': self.user4_first_name, 'email': self.user4_email,
+            'password1': self.user4_password1, 'password2': self.user4_password2})
+        self.assertContains(response, 'Passwörter stimmen nicht überein.')
--- app/views/auth.py
+++ app/views/auth.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 
-* Purpose :
+* Purpose : managing user account registration and login
 
 * Creation Date : 22-10-2014
 
@@ -9,7 +9,7 @@
 
 * Author :  maltsev
 
-* Coauthors : mattis
+* Coauthors : mattis, christian
 
 * Sprintnumber : 1
 
@@ -17,8 +17,11 @@
 
 from django.shortcuts import render,redirect
 from django.contrib import messages,auth
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from core.settings import LOGIN_URL
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 # see
 # https://docs.djangoproject.com/en/dev/topics/auth/default/#django.contrib.auth.login
@@ -46,10 +49,10 @@ def login(request):
                 messages.error(request, '{0} ist aus ausgesperrt.'.format(email))
 
         else:
-            messages.error(request, 'Email oder Passwort falsch.')
+            messages.error(request, 'E-Mail-Adresse oder Passwort falsch.')
 
 
-    return render(request, 'login.html', {'email': email})
+    return render(request, 'login.html')
 
 
 ## Logout
@@ -60,10 +63,60 @@ def logout(request):
     return redirect(LOGIN_URL)
 
 
-## Registration page
+## Default handler for registration requests by the client that sends the user the registration page.
+#  If correct registration details were sent with the request (over POST data), the user will be logged in
+#  and redirected to the start page
+#  Otherwise an error message will be inserted into the django messages queue.
 #  @param request The HttpRequest Object
 def registration(request):
+
     if request.user.is_authenticated():
         return redirect('/')
 
-    return render(request, 'registration.html', {'username': '', 'email': ''})
+    email = ''
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        # validation checks
+        # no empty fields
+        if first_name == '' or email == '' or password1 == '':
+            messages.error(request, 'Keine leeren Eingaben erlaubt.')
+        # email already registered
+        elif User.objects.filter(username=email).count() != 0:
+            messages.error(request, 'E-Mail-Adresse ist bereits registriert.')
+        # no valid email format
+        elif not validEmail(email):
+            messages.error(request, 'Ungültige E-Mail-Adresse.')
+        # first name can not only contain spaces
+        elif first_name.isspace():
+            messages.error(request, 'Vorname darf nicht nur aus Leerzeichen bestehen.')
+        # passwords do not match
+        elif password1 != password2:
+            messages.error(request, 'Passwörter stimmen nicht überein.')
+        # if all validation checks pass, create new user
+        else:
+            new_user = User.objects.create_user(username=email.lower(), email=email.lower(),
+                                                password=password1, first_name=first_name)
+
+            # user login and redirection to start page
+            user = auth.authenticate(username=email.lower(), password=password1)
+            if user is not None:
+                if user.is_active:
+                    auth.login(request, user)
+                    return redirect('/')
+            else:
+                messages.error(request, 'Anmeldung nach Registrierung fehlgeschlagen.')
+
+    return render(request, 'registration.html')
+
+
+# Helper function to check if a email address is valid
+def validEmail(email):
+    try:
+        validate_email(email)
+        return True
+    except ValidationError:
+        return False
