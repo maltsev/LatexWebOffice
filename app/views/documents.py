534137dd19cae9e9611ba03e7d855207c9c9b0b7
Mattis <github@mcom.moepl.eu>
2014-11-22 20:42:59 +0100

    Funktionialität für createDir, renameDir und rmDir hinzugefügt. rmDir funktioniert noch nicht, weil die Datenbank noch nicht richtig löscht

--- app/views/documents.py
+++ app/views/documents.py
@@ -4,7 +4,7 @@
 
 * Creation Date : 19-11-2014
 
-* Last Modified : Do 20 Nov 2014 14:38:06 CET
+* Last Modified : Sat 22 Nov 2014 02:49:28 PM CET
 
 * Author :  mattis
 
@@ -20,6 +20,12 @@ from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse
 # from models.project import Project
 import json
+import sys, traceback
+from app.models.folder  import Folder
+from app.models.project import Project
+from app.models.file.file import File
+from core.settings import ERROR_MESSAGES
+
 
 
 # Verteilerfunktion
@@ -28,7 +34,6 @@ import json
 @login_required
 def execute(request):
     if request.method == 'POST' and 'command' in request.POST:
-
         # hole den aktuellen Benutzer
         user = request.user
 
@@ -58,15 +63,18 @@ def execute(request):
         # Parameter dieses Befehls
         paras = c['parameters']
 
+
+        to_json = {
+            'status': 'failure',
+            'request': request.POST,
+            'response': ''
+            }
+
         # durchlaufe alle Parameter des Befehls
         for para in paras:
             # wenn der Parameter nicht gefunden wurde, gib Fehlermeldung zurück
             if not request.POST[str(para)]:
-                to_json = {
-                    'status': 'failure',
-                    'request': request.POST,
-                    'response': 'Fehlender Parameter {0}'.format(para)
-                }
+                to_json['response']='Fehlender Parameter {0}'.format(para)
                 return HttpResponse(json.dumps(to_json), content_type="application/json")
             # sonst füge den Parameter zu der Argumentliste hinzu
             else:
@@ -74,18 +82,15 @@ def execute(request):
 
         # versuche den übergebenen Befehl auszuführen
         try:
+
             return c['command'](request, user, *args)
         # wenn der Schlüssel nicht gefunden wurde
         # gib Fehlermeldung zurück
         except KeyError:
-            to_json = {
-                'status': 'failure',
-                'request': request.POST,
-                'response': 'Befehl nicht gefunden'
-            }
-
-            return HttpResponse(json.dumps(to_json), content_type="application/json")
-
+            to_json['response']= 'Befehl nicht gefunden'
+        #except:
+        #    to_json['response']=str(sys.exc_info()[0])
+        return HttpResponse(json.dumps(to_json), content_type="application/json")
 
 # liefert ein HTTP Response (Json)
 def jsonResponse(dictionary, status, request):
@@ -101,6 +106,9 @@ def jsonResponse(dictionary, status, request):
     }
 
     return HttpResponse(json.dumps(to_json), content_type="application/json")
+
+def jsonErrorResponse(errormsg,request):
+    return jsonResponse(errormsg,False,request)
 
 # aktualisiert eine geänderte Datei eines Projektes in der Datenbank
 # benötigt: id:fileid, content:filecontenttostring
@@ -149,7 +157,6 @@ def projectCreate(request, user, projectname):
         'reason': 'Projekt Erstellung erfolgreich'
     }
 
-    print(projectname)
 
     return HttpResponse(json.dumps(to_json), content_type="application/json")
 
@@ -181,32 +188,131 @@ def downloadFile(request, user, fileid):
 def exportZip(request, user, folderid):
     pass
 
+# Hilfsmethode um zu überprüfen, ob einer User überhaupt die Rechte hat einen Ordner zu bearbeiten und ob dieser Ordner existiert
+# benötigt: folderid, user, httprequest
+def checkIfDirExistsAndUserHasRights(folderid,user,request):
+    if not Folder.objects.filter(id=folderid).exists():
+        return False,jsonErrorResponse(ERROR_MESSAGES['DIRECTORYNOTEXIST'],request)
+    elif not Project.objects.get(id=Folder.objects.get(id=folderid).getRootFolder().id).author==user:
+        return False,jsonErrorResponse(ERROR_MESSAGES['NOTENOUGHRIGHTS'],request)
+    else:
+        return True,None
+
+def checkIfFileExistsAndUserHasRights(fileid,user,request):
+    if not File.objects.filter(id=fileid).exists():
+        return False,jsonErrorResponse(ERROR_MESSAGES['FILENOTEXIST'],request)
+    elif not File.objects.get(id=fileid).author==user:
+        return False,jsonErrorResponse(ERROR_MESSAGES['NOTENOUGHRIGHTS'],request)
+    else:
+        return True,None
+    
 
 # erstellt einen neuen Ordner im angegebenen Verzeichnis
-# benötigt: id:rootdirid, name:directoryname
+# benötigt: id:parentdirid, name:directoryname
 # liefert: HTTP Response (Json)
-def createDir(request, user, rootdirid, directoryname):
-    pass
+def createDir(request, user, parentdirid, directoryname):
+    #Check if parentdirid exists
+
+    rights,failurereturn=checkIfDirExistsAndUserHasRights(parentdirid,user,request)
+    if not rights:
+        return failurereturn
+
+
+    parentdir=Folder.objects.get(id=parentdirid) 
+
+
+    ################################
+    
+    
+    
+    #Test for empty strings
+    
+    
+    subfolders=parentdir.folder_set.all()
+    for f in subfolders:
+        if f.name==directoryname[:255]: #TODO diesen check muss die Datenbank machen!!!
+            return jsonErrorResponse(ERROR_MESSAGES['FOLDERNAMEEXISTS'],request)
+
+    ################################
+    
+    #Versuche den Ordner in der Datenbank zu speichern 
+    try:
+        newfolder=Folder(name=directoryname,parentFolder=parentdir)
+        newfolder.save()
+        return jsonResponse({'id':newfolder.id,'name':newfolder.name,'parentfolderid':parentdir.id,'parentfoldername':parentdir.name},True,request)
+    except:
+        return jsonErrorResponse(ERROR_MESSAGES['UNKOWNERROR'],request)
+
 
 
 # benennt den Ordner mit der angegebenen ID um
 # benötigt: id:folderid, name:newdirectoryname
 # liefert: HTTP Response (Json)
 def renameDir(request, user, folderid, newdirectoryname):
-    pass
+
+    rights,failurereturn=checkIfDirExistsAndUserHasRights(folderid,user,request)
+    if not rights:
+        return failurereturn
+
+    folder=Folder.objects.get(id=folderid) 
+
+    ################################
+
+
+
+    #Test for empty strings
+
+    subfolders=folder.folder_set.all()
+    if folder.parentFolder:
+        subfolders=folder.parentFolder.folder_set.all()
+    for f in subfolders:
+        if f.name==newdirectoryname[:255]: #TODO diesen check muss die Datenbank machen!!!
+            return jsonErrorResponse(ERROR_MESSAGES['FOLDERNAMEEXISTS'],request)
+
+    ################################
+    
+    try:
+        folder.name=newdirectoryname
+        folder.save()
+        return jsonResponse({'id':folder.id,'name':folder.name},True,request)
+    except:
+        return jsonErrorResponse(ERROR_MESSAGES['UNKOWNERROR'],request)
+
 
 
 # löscht den Ordner mit der angegebenen ID
 # benötigt: id:folderid
 # liefert: HTTP Response (Json)
 def rmDir(request, user, folderid):
-    pass
+
+    rights,failurereturn=checkIfDirExistsAndUserHasRights(folderid,user,request)
+    if not rights:
+        return failurereturn
+
+    folder=Folder.objects.get(id=folderid)
+    try:
+        folder.delete()
+        return jsonResponse({},True,request)
+    except:
+        return jsonErrorResponse(ERROR_MESSAGES['UNKOWNERROR'],request)
+
+
 
 # benötigt: id:fileid
 # liefert: HTTP Response (Json) --> fileid, filename, folderid, foldername
 def fileInfo(request, user, fileid):
-    pass
+#TODO funktioniert noch nicht
+   # rights,failurereturn=checkIfFileExistsAndUserHasRights(fileid,user,request)
+   # if not rights:
+   #     return failurereturn
 
+   # fileobj=File.objects.get(id=fileid)
+   # folder=Folder.objects.get(id=fileobj.folder.id)
+
+    #dictionary={'fileid':fileobj.id,'filename':fileobj.name,'folderid':folder.obj,'foldername':folder.name}
+
+    #return jsonResponse(dictionary,True,request)
+    pass
 
 # Kompiliert eine LaTeX Datei
 # benötigt: id:fileid
