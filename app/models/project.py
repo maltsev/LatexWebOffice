116600956d7aa33e30e7b705b989bdd8d54556e3
Kirill Maltsev <maltsevkirill@gmail.com>
2014-11-28 14:31:02 +0100

    Automatische Erstellung von Rootverzeichnise (Issue #9)

--- app/models/project.py
+++ app/models/project.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+import random, string
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
 from app.models.folder import Folder
 
@@ -22,3 +23,10 @@ class Project(models.Model):
 @receiver(post_delete, sender=Project)
 def projectPostDelete(instance, **kwargs):
     instance.rootFolder.delete()
+
+@receiver(pre_save, sender=Project)
+def projectPreSave(instance, **kwargs):
+    if not hasattr(instance, 'rootFolder') or not instance.rootFolder:
+        randomFolderName = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(32))
+        rootFolder = Folder.objects.create(name=randomFolderName)
+        instance.rootFolder = rootFolder
