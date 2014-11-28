cec5bd85d59f3c52266637fd09bc00d719ee4ac5
Kirill Maltsev <maltsevkirill@gmail.com>
2014-11-28 22:51:25 +0100

    BinaryFile aus File oder RequestFile erzeugen (Issue #7)

--- app/models/file/binaryfile.py
+++ app/models/file/binaryfile.py
@@ -1,16 +1,52 @@
 # -*- coding: utf-8 -*-
-import os
+import os, tempfile
 from django.db import models
 from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
 from app.models.file.file import File
 
 
+class BinaryFileManager(models.Manager):
+    def createFromRequestFile(self, **kwargs):
+        isFilepath = 'filepath' in kwargs and bool(kwargs['filepath'])
+        requestFile = 'requestFile' in kwargs and kwargs['requestFile']
+        if requestFile and not isFilepath:
+            _, filepath = tempfile.mkstemp()
+            newFile = open(filepath, 'wb')
+            for chunk in requestFile.chunks():
+                newFile.write(chunk)
+
+            newFile.close()
+
+            kwargs['filepath'] = filepath
+            del kwargs['requestFile']
+
+        return self.create(**kwargs)
+
+
+    def createFromFile(self, **kwargs):
+        isFilepath = 'filepath' in kwargs and bool(kwargs['filepath'])
+        file = 'file' in kwargs and kwargs['file']
+        if file and not isFilepath:
+            _, filepath = tempfile.mkstemp()
+            newFile = open(filepath, 'wb')
+            file.seek(0)
+            newFile.write(file.read())
+            newFile.close()
+
+            kwargs['filepath'] = filepath
+            del kwargs['file']
+
+        return self.create(**kwargs)
+
+
+
 class BinaryFile(File):
     filepath = models.CharField(max_length=255)
+    objects = BinaryFileManager()
 
     def getContent(self):
-        return open(str(self.filepath), 'r')
+        return open(str(self.filepath), 'rb')
 
 
 @receiver(post_delete, sender=BinaryFile)
--- app/tests/server/models/file/test_Binaryfile.py
+++ app/tests/server/models/file/test_Binaryfile.py
@@ -14,10 +14,14 @@
 
 """
 import tempfile, os
+from core.settings import BASE_DIR
 from app.models.file.binaryfile import BinaryFile
 from app.tests.server.models.modeltestcase import ModelTestCase
 
 class BinaryFileTestCase(ModelTestCase):
+    testBinaryFilepath = os.path.join(BASE_DIR, 'app/static/img/icon.png')
+
+
     def setUp(self):
         self.setUpProject()
 
@@ -25,12 +29,38 @@ class BinaryFileTestCase(ModelTestCase):
     def test_getContent(self):
         sourceCode = 'Straße, ändern, tést'
         _, filepath = tempfile.mkstemp()
-        file = open(filepath, 'w')
-        file.write(sourceCode)
+        file = open(filepath, 'wb')
+        file.write(bytearray(sourceCode, 'utf-8'))
         file.close()
 
         binaryFile = BinaryFile.objects.create(name='readme.txt', filepath=filepath, folder=self.rootFolder_dir1)
-        self.assertEqual(sourceCode, binaryFile.getContent().readline())
+        self.assertEqual(sourceCode, binaryFile.getContent().readline().decode('utf-8'))
         binaryFile.delete()
 
         self.assertFalse(os.path.isfile(filepath))
+
+
+    def test_createFromFile(self):
+        file = open(self.testBinaryFilepath, 'rb')
+        binaryFile = BinaryFile.objects.createFromFile(name='icon.png', folder=self.rootFolder_dir1, file=file)
+
+        file.seek(0)
+        self.assertEqual(file.read(), binaryFile.getContent().read())
+        binaryFile.delete()
+
+
+    def test_createFromRequestFile(self):
+        class RequestFileStub:
+            def __init__(self, filepath):
+                self.file = open(filepath, 'rb')
+
+            def chunks(self):
+                return [line for line in self.file]
+
+
+        requestFileStub = RequestFileStub(self.testBinaryFilepath)
+        binaryFile = BinaryFile.objects.createFromRequestFile(name='icon.png', folder=self.rootFolder_dir1, requestFile=requestFileStub)
+
+        file = open(self.testBinaryFilepath, 'rb')
+        self.assertEqual(file.read(), binaryFile.getContent().read())
+        binaryFile.delete()
