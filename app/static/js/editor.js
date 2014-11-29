b33f5d9dd710efb0e0320b09c4977ffae78ff576
Thore <t_thie06@wwu.de>
2014-11-29 23:47:13 +0100

    Header aktualisiert

--- app/static/js/editor.js
+++ app/static/js/editor.js
@@ -1,7 +1,7 @@
 /*
 @author: Thore Thießen, Timo Dümke
 @creation: 21.11.2014 - sprint-nr: 2
-@last-change: 25.11.2014 - sprint-nr: 2
+@last-change: 29.11.2014 - sprint-nr: 2
 */
 
 /// ID der im Editor geöffneten Datei
