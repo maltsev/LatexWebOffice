385485b6413553fdb6109fb89499ffaefe0676b6
Christian Lohmann <c_lohm05@wwu.de>
2014-12-12 15:45:01 +0100

    migration merge_py hinzugefügt

---
+++ app/migrations/0016_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0015_auto_20141210_1135'),
+        ('app', '0015_file_mimetype'),
+    ]
+
+    operations = [
+    ]
