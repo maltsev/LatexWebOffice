29000b2e94398e0b6d6636b99500234b8b6f0b85
Timo1993 <t_duem01@uni-muenster.de>
2014-12-12 14:38:14 +0100

    Dateiliste ergänzt2

---
+++ app/static/js/dateien.js
@@ -0,0 +1,66 @@
+﻿/*
+@author: Timo Dümke
+@creation: 12.12.2014 - sprint-nr: 3
+@last-change: 12.12.2014 - sprint-nr: 3
+*/
+var filelistHandler;
+$( document ).ready(function() {
+filelistHandler = new ListSelector("dateien");
+filelistHandler.setCaptions([
+{'name': 'Name', 'element': 'name'},
+{'name': 'Dateityp', 'element': 'filetype'}
+]);
+showFilelist(1);
+});
+
+function showFilelist(projectID){
+jQuery.ajax('/documents/', {
+		'type': 'POST',
+		'data': {
+			'command': 'listfiles',
+			'id': projectID
+		},
+		'headers': {
+			'X-CSRFToken': $.cookie('csrftoken')
+		},
+		'dataType': 'json',
+		'error': function(response, textStatus, errorThrown) {
+			//Anfrage Fehlerhaft
+			console.log({
+				'error': 'Fehler beim Abrufen der Dateiliste',
+				'details': errorThrown,
+				'id': projectID,
+				'statusCode': response.status,
+				'statusText': response.statusText
+			});
+		},
+		'success': function(data, textStatus, response) {
+			if (data.status != 'success'){
+				// Fehler auf dem Server
+				console.log({
+					'error': 'Fehler beim Abrufen der Dateiliste',
+					'details': data.response,
+					'statusCode': response.status,
+					'statusText': response.statusText
+				});
+			}
+			else {
+			// vorhandene Dateiliste entfernen
+			filelistHandler.clearData();
+			for (var i = 0; i < data.response.length; ++i){
+					if (i < data.response.length - 1){
+						filelistHandler.addData(data.response[i], [], false);
+						}
+					else{
+						filelistHandler.addData(data.response[i]);
+						}
+			
+			}
+			
+		}
+		}
+	});
+}
+
+
+
