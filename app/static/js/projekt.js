a790884899980d590185bb6a2e4de87d7b82fd3b
Thore <t_thie06@wwu.de>
2014-12-17 15:49:16 +0100

    Projekt Erstellen/Löschen-Funktionalität eingebunden

--- app/static/js/projekt.js
+++ app/static/js/projekt.js
@@ -21,6 +21,21 @@ $(document).ready(function() {
 	$('#open').click(function() {
 		if (projectList.getSelected() != null)
 			openProject(projectList.getSelected());
+		else
+			dialogNoSelection('Öffnen');
+	});
+
+	// Projekt erstellen
+	$('#create').click(function() {
+		dialogCreateProject();
+	});
+
+	// Projekt löschen
+	$('#delete').click(function() {
+		if (projectList.getSelected() != null)
+			dialogDeleteConfirmation(projectList.getSelected().id);
+		else
+			dialogNoSelection('Löschen');
 	});
 });
 
@@ -75,4 +90,137 @@ function showProjects() {
  */
 function openProject(project) {
 	document.location.assign('/dateien/#' + project.rootid);
+}
+
+/**
+ * Löscht ein Projekt.
+ * @param id ID des Projektes
+ */
+function deleteProject(id) {
+	jQuery.ajax('/documents/', {
+		'type': 'POST',
+		'data': {
+			'command': 'projectrm',
+			'id': id
+		},
+		'headers': {
+			'X-CSRFToken': $.cookie('csrftoken')
+		},
+		'dataType': 'json',
+		'error': function(response, textStatus, errorThrown) {
+			// Fehler beim Löschen
+			console.log({
+				'error': 'Fehler beim Löschen des Projektes',
+				'details': errorThrown,
+				'id': id,
+				'statusCode': response.status,
+				'statusText': response.statusText
+			});
+		},
+		'success': function(data, textStatus, response) {
+			if (data.status != 'success')
+				// Server-seitiger Fehler
+				console.log({
+					'error': 'Fehler beim Löschen des Projektes',
+					'details': data.response,
+					'id': id,
+					'statusCode': response.status,
+					'statusText': response.statusText
+				});
+			else
+				showProjects();
+		}
+	});
+}
+
+/**
+ * Erstellt ein neues Projekt.
+ * @param name Name des Projektes
+ */
+function createProject(name) {
+	jQuery.ajax('/documents/', {
+		'type': 'POST',
+		'data': {
+			'command': 'projectcreate',
+			'name': name
+		},
+		'headers': {
+			'X-CSRFToken': $.cookie('csrftoken')
+		},
+		'dataType': 'json',
+		'error': function(response, textStatus, errorThrown) {
+			// Fehler beim Erstellen
+			console.log({
+				'error': 'Fehler beim Erstellen des Projektes',
+				'details': errorThrown,
+				'name': name,
+				'statusCode': response.status,
+				'statusText': response.statusText
+			});
+		},
+		'success': function(data, textStatus, response) {
+			if (data.status != 'success')
+				// Server-seitiger Fehler
+				console.log({
+					'error': 'Fehler beim Erstellen des Projektes',
+					'details': data.response,
+					'name': name,
+					'statusCode': response.status,
+					'statusText': response.statusText
+				});
+			else
+				showProjects();
+		}
+	});
+}
+
+/**
+ * Zeigt einen Dialog mit dem Hinweis, dass keine Auswahl getroffen wurde, an.
+ * @param method Name der Funktion, die eine Auswahl benötigt
+ */
+function dialogNoSelection(method) {
+	// Funktion
+	$('#dialog_noSelection_method').text(method);
+
+	// OK-Button
+	$('#dialog_noSelection_ok').click(function() {
+		$('#dialog_noSelection').dialog('destroy');
+	});
+
+	$('#dialog_noSelection').dialog();
+}
+
+/**
+ * Zeigt einen Dialog an, der eine Bestätigung für das Löschen eines Projektes fordert.
+ * @param id ID des zu löschenden Projektes
+ */
+function dialogDeleteConfirmation(id) {
+	// Ja-Button
+	$('#dialog_deleteConfirmation_yes').click({'id': id}, function(event) {
+		deleteProject(event.data.id);
+		$('#dialog_deleteConfirmation').dialog('destroy');
+	});
+
+	// Nein-Button
+	$('#dialog_deleteConfirmation_no').click(function() {
+		$('#dialog_deleteConfirmation').dialog('destroy');
+	});
+
+	$('#dialog_deleteConfirmation').dialog();
+}
+
+/**
+ * Zeigt einen Dialog zum Erstellen eines neuen Projektes an.
+ */
+function dialogCreateProject() {
+	// Name zunächst leer
+	$('#dialog_createProject_name').val('');
+
+	// OK-Button
+	$('#dialog_createProject_ok').click(function() {
+		createProject($('#dialog_createProject_name').val());
+		$('#dialog_createProject').dialog('destroy');
+	});
+
+	$('#dialog_createProject').dialog();
 }
