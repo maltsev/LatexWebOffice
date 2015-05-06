47da5ba9d8483a3af156358dab28abe04bd15cd7
Kirill Maltsev <maltsevkirill@gmail.com>
2015-05-06 21:13:21 +0200

    admin.py downgrade

--- app/admin.py
+++ app/admin.py
@@ -18,85 +18,6 @@ from django import forms
 from django.contrib import admin
 from app import models
 
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
-
-from app.models.latexuserprofile import LatexWebUser as MyUser
-
-
-
-class UserCreationForm(forms.ModelForm):
-    """A form for creating new users. Includes all the required
-    fields, plus a repeated password."""
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
-
-    class Meta:
-        model = MyUser
-        fields = ('email', 'first_name')
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-
-class UserChangeForm(forms.ModelForm):
-    """A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    """
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = MyUser
-        fields = ('email', 'password', 'first_name', 'is_active', 'is_admin')
-
-    def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
-        return self.initial["password"]
-
-
-class MyUserAdmin(UserAdmin):
-    # The forms to add and change user instances
-    form = UserChangeForm
-    add_form = UserCreationForm
-
-    # The fields to be used in displaying the User model.
-    # These override the definitions on the base UserAdmin
-    # that reference specific fields on auth.User.
-    list_display = ('email', 'first_name', 'is_admin')
-    list_filter = ('is_admin',)
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_admin',)}),
-    )
-    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
-    # overrides get_fieldsets to use this attribute when creating a user.
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'first_name', 'password1', 'password2')}
-        ),
-    )
-    search_fields = ('email',)
-    ordering = ('email',)
-    filter_horizontal = ()
-
 
 
 class FolderForm(forms.ModelForm):
@@ -107,7 +28,7 @@ class FolderForm(forms.ModelForm):
         self.fields['root'].required = True
 
 
-@admin.register(models.folder.Folder)
+
 class FolderAdmin(admin.ModelAdmin):
     list_display = ('id', 'relativePath', 'createTime', 'parent', 'root')
     list_filter = ('parent', 'root')
@@ -118,21 +39,31 @@ class FolderAdmin(admin.ModelAdmin):
     relativePath.short_description = 'Path'
 
 
-@admin.register(models.collaboration.Collaboration)
+admin.site.register(models.folder.Folder, FolderAdmin)
+
+
+
 class CollaborationAdmin(admin.ModelAdmin):
     list_display = ('user', 'project', 'isConfirmed')
 
+admin.site.register(models.collaboration.Collaboration, CollaborationAdmin)
 
-@admin.register(models.projecttemplate.ProjectTemplate)
+
+
 class ProjectTemplateAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'author', 'createTime')
     list_filter = ('author',)
     exclude = ('rootFolder',)
 
+admin.site.register(models.projecttemplate.ProjectTemplate, ProjectTemplateAdmin)
 
-@admin.register(models.project.Project)
+
+
 class ProjectAdmin(ProjectTemplateAdmin):
     pass
+
+admin.site.register(models.project.Project, ProjectAdmin)
+
 
 
 class FileAdmin(admin.ModelAdmin):
@@ -148,24 +79,32 @@ class FileAdmin(admin.ModelAdmin):
     lock.boolean = True
 
 
-@admin.register(models.texfile.TexFile)
+
 class TexFileAdmin(FileAdmin):
     pass
 
+admin.site.register(models.texfile.TexFile, TexFileAdmin)
 
-@admin.register(models.image.Image)
+
+
 class ImageAdmin(FileAdmin):
     pass
 
+admin.site.register(models.image.Image, ImageAdmin)
 
-@admin.register(models.binaryfile.BinaryFile)
+
+
 class BinaryFileAdmin(FileAdmin):
     pass
 
+admin.site.register(models.binaryfile.BinaryFile, BinaryFileAdmin)
 
-@admin.register(models.plaintextfile.PlainTextFile)
+
+
 class PlainTextFileAdmin(FileAdmin):
     pass
+
+admin.site.register(models.plaintextfile.PlainTextFile, PlainTextFileAdmin)
 
 
 
