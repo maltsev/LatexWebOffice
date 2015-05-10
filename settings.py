8995db7362bb9506e2e0b049264bc4e1107e4d35
Kirill Maltsev <maltsevkirill@gmail.com>
2015-05-10 22:18:49 +0200

    local_settings erstellt

--- settings.py
+++ settings.py
@@ -125,7 +125,7 @@ LOGGING = {
 
 
 
-# Custom settings (non Django)
+# Project settings
 LOGIN_URL = '/login/'
 
 FILE_ROOT = os.path.join(MEDIA_ROOT, 'files')
@@ -149,3 +149,6 @@ if 'test' in sys.argv:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+
+
+from local_settings import *
