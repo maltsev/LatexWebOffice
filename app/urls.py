99bd1e79399a53141043ffbe8ebefe8d396fdacd
Kirill Maltsev <maltsevkirill@gmail.com>
2015-05-11 13:27:23 +0200

    URLs genannt

--- app/urls.py
+++ app/urls.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
-from django.views.generic.base import RedirectView
+
 
 urlpatterns = patterns('app.views',
-    url(r'^$', 'main.index'),
-    url(r'^registration/', 'auth.registration'),
-    url(r'^reguserexists/', 'auth.userexists'),
+    url(r'^$', 'main.index', name='index'),
+    url(r'^registration/', 'auth.registration', name='registration'),
+    url(r'^reguserexists/', 'auth.userexists', name='reguserexists'),
     url(r'^login/', 'auth.login', name='login'),
-    url(r'^logout/', 'auth.logout'),
+    url(r'^logout/', 'auth.logout', name='logout'),
     url(r'^recoverpw/', 'auth.lostPwHandler',name='recoverpw'),
-    url(r'^impressum/', 'main.impressum'),
-    url(r'^editor/', 'main.editor'),
-    url(r'^documents/', 'document.execute'),
-    url(r'^projekt/', 'main.projekt'),
-    url(r'^dateien/', 'main.dateien'),
-    url(r'^vorlagen/', 'main.vorlagen'),
-    url(r'^faq/', 'main.faq'),
-    url(r'^favicon.png', RedirectView.as_view(url='/static/img/favicon.png')),
-    url(r'^favicon.ico', RedirectView.as_view(url='/static/img/favicon.ico')),
+    url(r'^impressum/', 'main.impressum', name='impressum'),
+    url(r'^editor/', 'main.editor', name='editor'),
+    url(r'^documents/', 'document.execute', name='documents'),
+    url(r'^projekt/', 'main.projekt', name='projekt'),
+    url(r'^dateien/', 'main.dateien', name='dateien'),
+    url(r'^vorlagen/', 'main.vorlagen', name='vorlagen'),
+    url(r'^faq/', 'main.faq', name='faq'),
 )
 if settings.DEBUG:
     urlpatterns += patterns('app.views',
