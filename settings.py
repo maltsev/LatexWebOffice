c5700982d7935f686793d1dd29d7eebc23e109f6
Kirill Maltsev <maltsevkirill@gmail.com>
2015-05-18 15:45:07 +0200

    FILE_ROOT aus settings.py entfernt

--- settings.py
+++ settings.py
@@ -127,7 +127,6 @@ LOGGING = {
 
 
 # Project settings
-FILE_ROOT = os.path.join(MEDIA_ROOT, 'files')
 PROJECT_ROOT = os.path.join(MEDIA_ROOT, 'projects')
 TESTFILES_ROOT = os.path.join(BASE_DIR, 'app', 'tests', 'server', 'static')
 
@@ -135,7 +134,6 @@ TESTFILES_ROOT = os.path.join(BASE_DIR, 'app', 'tests', 'server', 'static')
 
 if 'test' in sys.argv:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'test')
-    FILE_ROOT = os.path.join(MEDIA_ROOT, 'files')
     PROJECT_ROOT = os.path.join(MEDIA_ROOT, 'projects')
 
     PASSWORD_HASHERS = (
